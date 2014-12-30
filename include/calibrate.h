af42698bb78f458df3cc09d03b2d0487aabaa921
dacb <dacb@comcast.net>
2014-12-30 12:29:21 -0800

    headers for temporary modules

---
+++ include/calibrate.h
@@ -0,0 +1,6 @@
+#ifndef __CALIBRATE_H__
+#define __CALIBRATE_H__
+
+void calibrate(void);
+
+#endif /* __CALIBRATE_H__ */
---
+++ include/km.h
@@ -0,0 +1,6 @@
+#ifndef __KM_H__
+#define __KM_H__
+
+void km_test(void);
+
+#endif // __KM_H__
